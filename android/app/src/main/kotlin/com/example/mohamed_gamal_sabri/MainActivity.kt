c4a755b201d8c0752351140a881170e1332b5793
devmohamedgamal <mpedo2010@gmail.com>
2024-01-11 07:43:57 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/mohamed_gamal_sabri/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mohamed_gamal_sabri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,14 @@
+import 'package:go_router/go_router.dart';
+import '../../features/home/presentation/views/home_view.dart';
+
+abstract class AppRouter {
+  static const String kHomeView = '/homeView';
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: kHomeView,
+        builder: (context, state) => const HomeView(),
+      ),
+    ],
+  );
+}
